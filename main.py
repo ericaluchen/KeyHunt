9557f3a5f3e773e6a3205d3003bb9a623737a0bb
ericaluchen <167537629+ericaluchen@users.noreply.github.com>
2024-05-09 09:26:49 -0400

    5/9/24 Day 3
    Set up the entire game (kept adding obstacles).
    *need to fix: character not moving*

--- main.py
+++ main.py
@@ -29,8 +29,20 @@ display_message = my_font.render(message, True,(255, 255, 255))
 #
 
 c = Character(0, 480)
-o = Obstacle(125, 450)
-k = Key(100, 50)
+o = Obstacle(130, 450)
+o1 = Obstacle(0, 400)
+o2 = Obstacle(260, 400)
+o3 = Obstacle(130, 350)
+o4 = Obstacle(0, 300)
+o5 = Obstacle(260, 300)
+o6 = Obstacle(130, 250)
+o7 = Obstacle(0, 200)
+o8 = Obstacle(260, 200)
+o9 = Obstacle(130, 150)
+o10 = Obstacle(0, 100)
+o11 = Obstacle(260, 100)
+
+k = Key(295, 90)
 d = Door(350, 485)
 f = Floor(0, 512)
 
@@ -44,7 +56,7 @@ y = 10 # a part of jumping
 while run:
 
     keys = pygame.key.get_pressed()  # checking pressed keys
-    if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+    if keys[pygame.Kw_d] or keys[pygame.K_RIGHT]:
         c.move_direction("right")
     if keys[pygame.K_a] or keys[pygame.K_LEFT]:
         c.move_direction("left")
@@ -68,6 +80,18 @@ while run:
     screen.blit(display_message, (0, 0))
     screen.blit(c.image, c.rect)
     screen.blit(o.image, o.rect)
+    screen.blit(o1.image, o1.rect)
+    screen.blit(o2.image, o2.rect)
+    screen.blit(o3.image, o3.rect)
+    screen.blit(o4.image, o4.rect)
+    screen.blit(o5.image, o5.rect)
+    screen.blit(o6.image, o6.rect)
+    screen.blit(o7.image, o7.rect)
+    screen.blit(o8.image, o8.rect)
+    screen.blit(o9.image, o9.rect)
+    screen.blit(o10.image, o10.rect)
+    screen.blit(o11.image, o11.rect)
+
     screen.blit(k.image, k.rect)
     screen.blit(d.image, d.rect)
     screen.blit(f.image, f.rect)
